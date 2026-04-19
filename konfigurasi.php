efe1365fde4af73f5e301a7602d9ea5a5316e1c1
Ash <96473011+xdr7@users.noreply.github.com>
2026-04-19 09:52:46 +0800

    Update konfigurasi.php
    Signed-off-by: Ash <96473011+xdr7@users.noreply.github.com>

--- konfigurasi.php
+++ konfigurasi.php
@@ -4,7 +4,7 @@
 	$pass	= "";
 	$db		= "insreport";
 
-	$site_url = "http://192.168.35.50:7777/insreport/";
+	$site_url = "http://localhost/insreport/";
 	
 	$con 	= mysqli_connect($host, $user, $pass) or die (mysqli_error());
 	$db	 	= mysqli_select_db($con, $db) or die(mysqli_error());
